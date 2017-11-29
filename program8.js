fcb0881aee1b91fad6f6b904ff8a5d9981cb4c2b
tabbu724 <tabbusehgal724@gmail.com>
2017-11-29 20:03:31 +0530

    collecting all data from server using bl

--- program8.js
+++ program8.js
@@ -10,9 +10,18 @@ http.get(url , function(response){
   }))
 })
 
-/*http.get(url , function(response){
-  response.setEncoding('utf8');
-  response.on('data' , function(data){
-    console.log(data);
-  });
-});*/
+/*----------------Official solution-------------
+var http = require('http')
+    var bl = require('bl')
+
+    http.get(process.argv[2], function (response) {
+      response.pipe(bl(function (err, data) {
+        if (err) {
+          return console.error(err)
+        }
+        data = data.toString()
+        console.log(data.length)
+        console.log(data)
+      }))
+    })
+*/
