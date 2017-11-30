3c541269357c234412ccfea1b25d3645feb9a9bb
tabbu724 <tabbusehgal724@gmail.com>
2017-11-30 19:00:57 +0530

    http stream transformer(to uppercase)

---
+++ program12.js
@@ -0,0 +1,26 @@
+var http = require('http');
+var map = require('through2-map');
+var server = http.createServer(function callback(request , response){
+request.pipe(map(function (chunk){
+  return chunk.toString().toUpperCase();
+})).pipe(response);
+});
+server.listen(process.argv[2]);
+
+/*-------------Official solution--------
+var http = require('http')
+    var map = require('through2-map')
+
+    var server = http.createServer(function (req, res) {
+      if (req.method !== 'POST') {
+        return res.end('send me a POST\n')
+      }
+
+      req.pipe(map(function (chunk) {
+        return chunk.toString().toUpperCase()
+      })).pipe(res)
+    })
+
+    server.listen(Number(process.argv[2]))
+
+*/
